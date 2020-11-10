4a60f5b5eb6fbff49d7f8fbaf695e6d886b5a7cf
DFBL <63087864+dfblhmmpp@users.noreply.github.com>
2020-11-10 23:36:49 +0800

    Add files via upload

---
+++ animate.js
@@ -0,0 +1,12 @@
+function animate(obj,target,callback) {
+    obj.timer = setInterval(function () {
+        var step = (target - obj.offsetLeft) / 10;
+        step = step > 0 ? Math.ceil(step) : Math.floor(step);
+        if (obj.offsetLeft === target) {
+            clearInterval(obj.timer);
+            if (callback)callback();
+        } else {
+            obj.style.left = obj.offsetLeft + step + 'px';
+        }
+    },15);
+}
